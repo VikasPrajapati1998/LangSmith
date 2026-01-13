946cbde4ec4c8614fe02c68dca430ec3289b1872
VikasPrajapati1998 <vikasprajapati191198@gmail.com>
2026-01-13 16:39:47 +0530

    langsmith

---
+++ 1_Simple_LLM_Call.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+# Simple one-line prompt
+prompt = PromptTemplate.from_template("{question}")
+
+model = ChatOllama(
+    model="llama3.2:1b",   # llama3.2:1b, qwen2.5:0.5b
+    temperature=0.3
+)
+parser = StrOutputParser()
+
+# Chain: prompt → model → parser
+chain = prompt | model | parser
+
+# Run it
+result = chain.invoke({"question": "What is the capital of Peru?"})
+print(result)
+
